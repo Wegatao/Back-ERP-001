19c28bdb387d45c73cab541850fb06e4b0449eb3
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-06-09 19:30:07 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 
+CONFING = {
+            "host": "br268.hostgator.com.br",
+            "user": "welber77_welber",
+            "password": "1Ddeprs1#%",
+            "database": "welber77_root"
+        }
+
