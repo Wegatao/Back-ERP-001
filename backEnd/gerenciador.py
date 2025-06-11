799a47efcf20451085718ca7446c448e713d6ed8
Wegatao <willianwelber11@gmail.com>
2025-06-11 12:57:19 -0300

    Atualizando

--- backEnd/gerenciador.py
+++ backEnd/gerenciador.py
@@ -1,70 +1,77 @@
 import mysql.connector
 from mysql.connector import Error
-
+       
 # Classe que gerencia a conexão e operações com o banco de dados
 class GerenciadorCooperados:
-    def _init_(self):
-       def _init_(self, config):
-        self.config = config # Configuração do banco de dados
+       def __init__(self, config):
+         self.config = config # Configuração do banco de dados
          
-
-    # Conecta ao banco de dados
-    def conectar(self):
-        try:
+       
+       # Conecta ao banco de dados
+       def conectar(self):
+         try:
             conexao = mysql.connector.connect(**self.config)
             if conexao.is_connected():
                 print("Conexão bem-sucedida com o banco de dados.")
-            return conexao
-        except Error as e:
-            print(f"Erro ao conectar no MySQL: {e}")
-            return None
-
-    # Cria a tabela "cooperados" se ela não existir
-    def criar_tabela_PSS(self):
-        conexao = self.conectar()
-        if conexao:
+                return conexao
+         except Error as e:
+                print(f"Erro ao conectar no MySQL: {e}")
+                return None
+       
+       # Cria a tabela "cooperados" se ela não existir
+       def criar_tabela_PSS(self):
+         conexao = self.conectar()
+         if conexao:
             cursor = conexao.cursor()
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS PSS (
-                    Matricula VACHAR(20) PRYMARY KEY,
-                    nome VARCHAR(20) NOT NULL,
+                    Matricula VARCHAR(20) PRIMARY KEY
+                    nome VARCHAR(20) NOT NULL 
                 )
             """)
             conexao.commit()
             conexao.close()
-            
-    def criar_tabela_Pedencia(self):
+     
+       def criar_tabela_Pedencia(self):
         conexao = self.conectar()
         if conexao:
             cursor = conexao.cursor()
             cursor.execute("""
-                CREATE TABLE IF NOT EXISTS PSS (
+                CREATE TABLE IF NOT EXISTS Pedencia (
                     IdPedencias INT AUTO_INCREMENT PRIMARY KEY,
                     Matricula VARCHAR(20),
                     TipoPendencia VARCHAR(100),
-                    SatatusPendecia VARCHAR(20),
-                    DataTyme DATE,
+                    StatusPendecia VARCHAR(20),
+                    Data DATE,
                     Descricao VARCHAR(100),
                     FOREIGN KEY(Matricula) REFERENCES PSS(Matricula)
-                )
-            """)
+                )""")
             conexao.commit()
             conexao.close()
-
-    # Cadastra um novo cooperado no banco de dados
-    def cadastrar_cooperado(self, Matricula, nome):
+       
+       # Cadastra um novo cooperado no banco de dados
+       def cadastrar_PSS(self, Matricula, nome):
         conexao = self.conectar()
         if conexao:
             cursor = conexao.cursor()
             cursor.execute(
-                "INSERT INTO cooperados (Matricula, nome) VALUES (%s, %s)",
-                (Matricula, nome)
+                "INSERT INTO PSS (Matricula, nome) VALUES (%s, %s)", (Matricula, nome)
             )
             conexao.commit()
             conexao.close()
-
-    # Busca cooperados pelo nome
-    def buscar_cooperados(self, nome):
+           
+       def cadastrar_pendencia(self, Matricula, TipoPendencia, Status, Data, Descricao):
+          conexao = self.conectar()
+          if conexao:
+            cursor = conexao.cursor()
+            cursor.execute("INSERT INTO Pedencia (Matricula, TipoPendencia, Status, Data, Descricao)VALUES (%s, %s, %s, %s, %s)", 
+            (Matricula, TipoPendencia, Status, Data, Descricao)
+            )
+            conexao.commit()
+            conexao.close()
+                   
+       # Busca cooperados pelo nome
+       def buscar_cooperados(self, nome):
         conexao = self.conectar()
         cooperados = []
         if conexao:
@@ -73,9 +80,9 @@ class GerenciadorCooperados:
             cooperados = cursor.fetchall()
             conexao.close()
         return cooperados
-
-    # Atualiza dados de um cooperado
-    def atualizar_cooperado(self, id_cooperado, pendencias, data_emissao, observacao):
+       
+       # Atualiza dados de um cooperado
+       def atualizar_cooperado(self, id_cooperado, pendencias, data_emissao, observacao):
         conexao = self.conectar()
         if conexao:
             cursor = conexao.cursor()
