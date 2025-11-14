2bd8a68c816f55d90881eaeb0f21fc39fa8340bd
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-11-14 13:50:19 -0300

    Update gerenciador.py

--- gerenciador.py
+++ gerenciador.py
@@ -1,163 +1,93 @@
-import mysql.connector
-from mysql.connector import Error
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from datetime import datetime
-       
-# Classe que gerencia a conexão e operações com o banco de dados
-class GerenciadorCooperados:
-       def __init__(self, config):
-         self.config = config # Configuração do banco de dados
-         
-       
-       # Conecta ao banco de dados
-       def conectar(self):
-         try:
-            conexao = mysql.connector.connect(**self.config)
-            if conexao.is_connected():
-                print("Conexão bem-sucedida com o banco de dados.")
-                return conexao
-         except Error as e:
-                print(f"Erro ao conectar no MySQL: {e}")
-                return None
-       
-       # Cria a tabela "cooperados" se ela não existir
-       def criar_tabela_PSS(self):
-         conexao = self.conectar()
-         if conexao:
-            cursor = conexao.cursor()
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS PSS (
-                    Matricula VARCHAR(20) PRIMARY KEY,
-                    nome VARCHAR(20) NOT NULL
-                )
-            """)
-            conexao.commit()
-            conexao.close()
-     
-       def criar_tabela_Pendencia(self):
-        conexao = self.conectar()
-        if conexao:
-            cursor = conexao.cursor()
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS Pendencias (
-                    IdPedencias INT AUTO_INCREMENT PRIMARY KEY,
-                    Matricula VARCHAR(20),
-                    TipoPendencia VARCHAR(100),
-                    StatusPendecia VARCHAR(20),
-                    Data date,
-                    Descricao VARCHAR(100),
-                    FOREIGN KEY(Matricula) REFERENCES PSS(Matricula)
-                )""")
-            conexao.commit()
-            conexao.close()
-       
-       # Cadastra um novo cooperado no banco de dados
-       def cadastrar_PSS(self, Matricula, nome):
-        conexao = self.conectar()
-        if conexao:
-            cursor = conexao.cursor()
-            cursor.execute(
-                "INSERT INTO PSS (Matricula, nome) VALUES (%s, %s)", (Matricula, nome)
-            )
-            conexao.commit()
-            conexao.close()
-           
-       def cadastrar_pendencia(self, Matricula, TipoPendencia, StatusPendecia,Data, Descricao):
-          conexao = self.conectar()
-          # Inverte a data para o formato YYYY-MM-DD
-          Data = self.inverterData(Data)
-          if conexao:
-            try:
-              cursor = conexao.cursor()
-              cursor.execute("""
-                INSERT INTO Pendencias (Matricula, TipoPendencia, StatusPendecia, Data, Descricao)
-                VALUES (%s, %s, %s, %s, %s)
-                """, (Matricula, TipoPendencia, StatusPendecia, Data, Descricao))
-              conexao.commit()
-              return {"sucesso": True, "mensagem": "Pendência cadastrada com sucesso"}
-            except Error as e:
-              print(f"Erro ao cadastrar pendência: {e}")
-              return {"sucesso": False, "mensagem": f"Erro: {e}"}
-            finally:
-              conexao.close()
-            
-       def inverterData(self,i):
-          data_obj = datetime.strptime(i, "%d/%m/%Y")  # Converte string em data
-          data_convertida = data_obj.strftime("%Y-%m-%d")  # Converte data em string no formato certo
-          return data_convertida
-          
-                                      
-       # Busca cooperados pelo nome
-       def buscar_cooperados(self, nome):
-        cooperados = []
-        conexao = self.conectar()
+from gerenciador import GerenciadorCooperados
+from CadastrarPendencia import CadastrarPendencia
+from CadastrarPessoa import CadastrarPessoa
+from config import CONFING
+import os
 
-        if not conexao:
-         return cooperados
+app = Flask(__name__)
+CORS(app)
 
-        try:
-          
-          #Criado cursor para retornar dicionário. E executado a variável sql com parametro nome.
-          cursor = conexao.cursor(dictionary=True)
-          cursor.execute("""
-              SELECT 
-              p.Matricula,
-              p.nome,
-              pe.IdPedencias,
-              pe.TipoPendencia,
-              pe.StatusPendecia,
-              pe.Data,
-              pe.Descricao
-              FROM PSS p
-              INNER JOIN Pendencias pe ON p.Matricula = pe.Matricula
-              WHERE LOWER(p.nome) LIKE %s """,("%"+nome+"%"))  
-          
+gerenciador = GerenciadorCooperados(CONFING)
 
-          resultado = cursor.fetchall()
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"mensagem": "Backend do ERP está online!"})
+    
+@app.route("/cadastrarPessoa", methods=["POST"])
+def cadastrarPessoa():
+    dados = request.get_json()
+    resultado = CadastrarPessoa(dados).CadastraPessoaMethod()
+    return jsonify(resultado), 200 if resultado.get("sucesso") else 400
+
+@app.route("/cadastrarPendencia", methods=["POST"])
+def cadastrarPendencia():
+    dados = request.get_json()
+    resultado = CadastrarPendencia(dados).CadastrarPendenciaMethod()
+    return jsonify(resultado), 200 if resultado.get("sucesso") else 400
+
+
+@app.route("/buscar", methods=["POST"])
+def buscar():
+    dados = request.get_json()
+    print("DADOS RECEBIDOS:", dados, flush=True)
+    
+    if dados is None:
+        return jsonify({"sucesso": False, "mensagem": "JSON inválido ou não enviado."}), 400
+
+    nome = dados.get('nome').strip()
+    
+    if not nome:
+        return jsonify({"sucesso": False, "mensagem": "nome não fornecido."}), 400
       
+    print(f"Nome recebido para busca: {nome}")  # ✅ Log do nome recebido
+    resultado = gerenciador.buscar_cooperados(nome)  # ✅ Agora sim: passa apenas a string "nome"
+   
+    cooperados = [
+      {
+        "IdPedencias": row["IdPedencias"],
+        "id": row["Matricula"],
+        "nome": row["nome"],
+        "pendencias": row["TipoPendencia"],       
+        "StatusPedencia": row["StatusPendecia"],  
+        "observacao": row["Descricao"],
+        "data_emissao": row["Data"],
+      }
+        for row in resultado
+     ]
+    return jsonify({"cooperados": cooperados}), 200
 
-        #Retorna os dados eonctrado na busca.
-          if resultado:
-            cooperados = [
-                {
-                    "IdPedencias": row["IdPedencias"],
-                    "Matricula": row["Matricula"],
-                    "nome": row["nome"],
-                    "TipoPendencia": row["TipoPendencia"],
-                    "StatusPendecia": row["StatusPendecia"],
-                    "Data": row["Data"],
-                    "Descricao": row["Descricao"]
-                }
-                for row in resultado
-            ]
-          else:
-            print("Nenhum cooperado encontrado.")
-        
 
-        except Error as e:
-          print(f"Erro ao buscar cooperados: {e}")
-        finally:
-            if conexao.is_connected():  #garante que a conexão existe antes de fechar
-              conexao.close()
-        return cooperados
 
-       # Atualiza dados de um cooperado
-       def atualizar_pendencia(self, IdPendencia, PessoaAutorizada, AssinaturaCooperado):
-            conexao = self.conectar()
-            if conexao:
-              try:
-                cursor = conexao.cursor()
-                cursor.execute("""
-                  UPDATE Pendencias 
-                   SET StatusPendecia = %s, TipoPendencia = %s
-                   WHERE IdPedencias = %s           
-                  """, (PessoaAutorizada, AssinaturaCooperado, IdPendencia))
+@app.route("/atualizar", methods=["PUT"])
+def atualizar():
+    dados = request.get_json();
+    IdPendencia = dados.get("IdPedencias");# Matricula usada como ID
+    PessoaAutorizada = dados.get("PessoaAutorizada");
+    AssinaturaCooperado = dados.get("AssinaturaCooperado");
 
-                conexao.commit()  # ✅ commit vem antes do close
-                print("Pendência atualizada com sucesso!")
+    if not PessoaAutorizada or not AssinaturaCooperado :
+        return jsonify({"sucesso": False, "mensagem": "Campos obrigatórios não informados."})
 
-              except Error as e:
-                print(f"Erro ao atualizar pendência: {e}")
+    gerenciador.atualizar_pendencia(IdPendencia, PessoaAutorizada, AssinaturaCooperado);
+    
+    return jsonify({"sucesso": True, "mensagem": "Pendência atualizada com sucesso"})
 
-              finally:
-                if conexao.is_connected():
-                  conexao.close()
+
+#Essa função ainda não estpa criada.
+@app.route("/deletar", methods=["DELETE"]) 
+def deletar():
+    dados = request.get_json()
+    matricula = dados.get("id")
+    if not matricula:
+        return jsonify({"sucesso": False, "mensagem": "Matricula não informada."})
+    gerenciador.deletar_pendencia(matricula) 
+    return jsonify({"sucesso": True, "mensagem": "Pendência deletada com sucesso"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
