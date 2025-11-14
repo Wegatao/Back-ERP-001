4ce390445a077175d823a897c046bfba7e557889
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-11-14 18:17:04 -0300

    Update database user in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 CONFING = {
             "host": "br268.hostgator.com.br",
-            "user": "welber77_welber",
+            "user": "welber77_jogador",
             "password": "#Velaci*(00)1111",
             "database": "welber77_root"
         }
