06aa80040b241334e43d31e7e199bb1fb401a447
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-11-15 10:36:35 -0300

    Change database password in config.py
    Updated database password for security reasons.

--- backEnd/config.py
+++ backEnd/config.py
@@ -2,6 +2,6 @@
 CONFING = {
             "host": "br268.hostgator.com.br",
             "user": "welber77_welber",
-            "password": "Yeshua77*w",
+            "password": "#Velaci*(00)1111",
             "database": "welber77_root"
         }
