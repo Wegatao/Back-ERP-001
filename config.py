f0f3a4c6b07b40718f152bfe1b645350904987e0
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-11-15 09:18:13 -0300

    Fix typo in user configuration

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 CONFING = {
             "host": "br268.hostgator.com.br",
-            "user": "welber77_weeee",
+            "user": "welber77_weee",
             "password": "#Velaci*(00)1111",
             "database": "welber77_a"
         }
